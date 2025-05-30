0c5fa5a5cbef7daedb2c96fffcf766efa094e265
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-30 15:18:35 +0530

    creation of class and object example added

---
+++ oops1.py
@@ -0,0 +1,20 @@
+# creating a class
+class Student:
+  name = "Sibangi"
+
+#creating object or instance of class
+s1 = Student()
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
+
+class Car:
+  color = "White"
+  brand = "Mercedes"
+  owner = "Reena"
+
+car1 = Car()
+print(car1.brand)
+print(car1.color)
+print(car1.owner)
