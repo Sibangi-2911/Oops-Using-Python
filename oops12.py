c66b5410a9477ade5bf436ebdb2284c7a389d4b8
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-23 23:28:24 +0530

    inheritance example

---
+++ oops12.py
@@ -0,0 +1,20 @@
+#Inheritance
+class Car:
+  color = "Black"
+  @staticmethod
+  def start():
+    print("Car started...")
+  @staticmethod
+  def stop():
+    print("Car Stopped.")
+
+class ToyotaCar(Car):
+  def __init__(self, name):
+    self.name = name
+
+Car1 = ToyotaCar("Fortuner")
+Car2 = ToyotaCar("prius")
+print(Car1.name)
+print(Car1.start())
+print(Car2.stop())
+print(Car1.color)
